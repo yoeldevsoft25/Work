1abd6c1fc9c1eb0f74859b7f5d409cd3426735d0
YoelDevSoft1 <yoeldevsoft@gmail.com>
2025-05-21 02:38:59 -0600

    a

--- types/express.ts
+++
@@ -1,7 +0,0 @@
-// backend/types/express.ts
-import { Request } from 'express';
-import { IUserDocument } from '../models/User'; // Asegúrate que User.ts exporta esto
-
-export interface UserRequest extends Request {
-  user?: IUserDocument;
-}
